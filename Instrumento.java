a5b1559478af9e8d5fab8791ac92421a1a4ff2ea
Pau <mapaula2408@gmail.com>
2026-09-03 18:59:25 -0500

    Creacion método constructor

--- Instrumento.java
+++ Instrumento.java
@@ -5,4 +5,25 @@ public class Instrumento {
     private double precioInstrumento;
     private boolean esElectricoInstrumento;
 
+    public Instrumento(String nombreInstrumento, int anioFabricacion, double precioInstrumento, boolean esElectricoInstrumento){
+
+        this.nombreInstrumento = nombreInstrumento;
+        this.anioFabricacion = anioFabricacion;
+        this.precioInstrumento = precioInstrumento;
+        this.esElectricoInstrumento = esElectricoInstrumento;
+    }
+
+    String getNombreInstrumento(){
+        return nombreInstrumento;
+    }
+    int getAnioFabricacion(){
+        return anioFabricacion;
+    }
+    double getPrecioInstrumento(){
+        return precioInstrumento;
+    }
+    boolean getEsElectricoInstrumento(){
+        return esElectricoInstrumento;
+    }
+
 }   
